89e1b8ebac7dccfbf433a2dc4d0e1de92ea0d537
Mayuresh Tuplondhe <mayureshtuplondhe@gmail.com>
2026-09-13 10:05:25 +0530

    update

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
 async function connectDB() {
-  const MONGODB_URI = "mongodb+srv://dailyrpg_user:skeHZuX2Hkw0dM9D@cluster0.xxxxx.mongodb.net/DailyRPG?retryWrites=true&w=majority";
   const uri = process.env.MONGODB_URI;
   if (!uri) {
     throw new Error("MONGODB_URI is not set. Copy .env.example to .env and fill it in.");
